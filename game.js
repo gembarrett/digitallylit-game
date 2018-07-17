e99fbbc7ef166689a1093559e1c62b1730713bf6
Gemma Barrett <geminimac@geminimac.local>
2018-07-17 13:18:40 -0400

    score updates, started text array

--- game.js
+++ game.js
@@ -8,18 +8,30 @@ var score = 0;
 var scoreText = document.getElementById('score');
 var round = 1;
 
+var array = [
+  "Black and white cat",
+  "Tortoiseshell cat",
+  "Grey cat",
+  "Ginger cat"
+];
+
 function handleAnswer(id){
   if (id === 'first') {
     alert('you chose the first option!');
-    score += 13;
+    score = score + 13;
     updateScore(score);
   } else {
     alert('you chose the second option!');
-    score += 5;
-    updateScore(5);
+    score = score + 5;
+    updateScore(score);
   }
 }
 
 function updateScore(score) {
   scoreText.innerText = score;
+}
+
+function updateText(current) {
+  firstChoice.innerText = array[current+2];
+  secondChoice.innerText = array[current+1];
 }
