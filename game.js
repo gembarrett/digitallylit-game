2ad5e81676d2d48099b5cb540737800ffd84c073
Gemma Barrett <geminimac@geminimac.local>
2018-07-17 13:03:02 -0400

    simpified the game, styled buttons a bit and added score-keeping

--- game.js
+++ game.js
@@ -4,11 +4,22 @@ function changeImage(src) {
 
 var firstChoice = document.getElementById('first');
 var secondChoice = document.getElementById('second');
+var score = 0;
+var scoreText = document.getElementById('score');
+var round = 1;
 
 function handleAnswer(id){
   if (id === 'first') {
     alert('you chose the first option!');
+    score += 13;
+    updateScore(score);
   } else {
     alert('you chose the second option!');
+    score += 5;
+    updateScore(5);
   }
+}
+
+function updateScore(score) {
+  scoreText.innerText = score;
 }
